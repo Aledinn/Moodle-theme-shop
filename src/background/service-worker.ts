b8698e3659443692da98baf1db9de4a17c8091dd
Aledinn <Aledinn@users.noreply.github.com>
2026-05-19 22:16:10 +0200

    add detect url for css inject

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -3,25 +3,29 @@ let editedTabs: number[] = [];
 
 chrome.action.onClicked.addListener((tab) => {
   if (!tab.id) return;
-  if (tab && !editedTabs.includes(tab.id)) {
-    editedTabs.push(tab.id);
-    chrome.scripting.insertCSS({
-        target: { tabId: tab.id },
-        css : css,
-    }).then(() => {
-    console.log('CSS injected successfully.');
-        });
-  }
-  else {
-    let index = editedTabs.indexOf(tab.id);
-    if (index > -1) {
-        editedTabs.splice(index, 1);   
+  console.log(tab.url)
+  if(tab.url?.startsWith("https://moodle.")) {
+    console.log("Moodle page detected");
+    if (tab && !editedTabs.includes(tab.id)) {
+        editedTabs.push(tab.id);
+        chrome.scripting.insertCSS({
+            target: { tabId: tab.id },
+            css : css,
+        }).then(() => {
+        console.log('CSS injected successfully.');
+            });
     }
-    chrome.scripting.removeCSS({
-        target: { tabId : tab.id },
-        css : css,
-    }).then(() => {
-    console.log('CSS removed successfully.');
-        });
+    else {
+        let index = editedTabs.indexOf(tab.id);
+        if (index > -1) {
+            editedTabs.splice(index, 1);   
+        }
+        chrome.scripting.removeCSS({
+            target: { tabId : tab.id },
+            css : css,
+        }).then(() => {
+        console.log('CSS removed successfully.');
+            });
+    }
   }
 });

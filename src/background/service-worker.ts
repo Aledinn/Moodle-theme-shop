169f81ddddf93a3409082275e1d17bd225c9eff9
Aledinn <Aledinn@users.noreply.github.com>
2026-05-26 22:24:22 +0200

    updated and refactored toggle_theme and install_theme

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -1,18 +1,27 @@
 import { toggleCSS, themeToCss } from "./css-injection";
 import { loadExampleTheme } from "./theme-loader";
-import { initializeStorage, upsertThemeForSite } from "./theme-storage";
-import { getCurrentTab, isInScope } from "./utils";
+import { initializeStorage, upsertThemeForSite,  getThemeForSite} from "./theme-storage";
+import { getCurrentTab, isInScope, getDomainFromUrl } from "./utils";
 
 initializeStorage();
 
 let css = 'body { border: 20px solid black; }';
-let allowedUrls: string[] = ["https://moodle."];
+let allowedUrls: string[] = ["moodle.informatik.tu-darmstadt.de"];
 let appliedUrls: string[] = [];
 
 //load css if url already in scope
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && isInScope(tab.url!,appliedUrls)) {
-      toggleCSS(tab, css, true);
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if(changeInfo.status !== 'complete' || !tab.url) {
+    return;
+  }
+  let tabUrl = getDomainFromUrl(tab.url);
+  if (!tabUrl) {
+    return;
+  }
+  let theme = await getThemeForSite(tabUrl);
+  if (isInScope(tabUrl, appliedUrls) && theme) {
+    css = themeToCss(theme);
+    toggleCSS(tab, css, true);
   }
 });
 
@@ -20,16 +29,22 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 chrome.runtime.onMessage.addListener((message, sender) => {
     if (message.type === "toggle_theme") {
         getCurrentTab().then((tab) => {
-        if  (!tab || !tab.id || !tab.url ||!isInScope(tab.url!, allowedUrls)){
+        if  (!tab || !tab.id || !tab.url){
             return;
         }
-        else if (!isInScope(tab.url!, appliedUrls)) {
+
+        const domain = getDomainFromUrl(tab.url);
+        if (!domain || !isInScope(domain, allowedUrls)) {
+            return;
+        }
+
+        if (!isInScope(domain, appliedUrls)) {
             toggleCSS(tab, css, true);
-            appliedUrls.push(tab.url);
+            appliedUrls.push(domain);
         }
         else {
             toggleCSS(tab, css, false);
-            appliedUrls = appliedUrls.filter((url) => url !== tab.url);
+            appliedUrls = appliedUrls.filter((url) => url !== domain);
         }
     }
     );}
@@ -57,11 +72,17 @@ chrome.runtime.onMessage.addListener(async (message, sender) => {
         await upsertThemeForSite(theme);
         css = themeToCss(theme);
         getCurrentTab().then((tab) => {
-            if (!tab || !tab.id || !tab.url ||!isInScope(tab.url!, allowedUrls)){
+            if (!tab || !tab.id || !tab.url){
                 return;
             }
+
+            const domain = getDomainFromUrl(tab.url);
+            if (!domain || !isInScope(domain, allowedUrls)) {
+                return;
+            }
+
             toggleCSS(tab, css, true);
-            appliedUrls.push(tab.url);
+            appliedUrls.push(domain);
         });
     }
 });

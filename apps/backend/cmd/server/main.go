a7ead5908485489afee43908b550a413f79cfb26
Aledinn <Aledinn@users.noreply.github.com>
2026-06-13 23:09:20 +0200

    project restructure and first backend code

--- apps/backend/cmd/server/main.go
+++ apps/backend/cmd/server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("test")
+	fmt.Println("server listening on :8080")
+	http.HandleFunc("/health", handleHealth)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		log.Fatal(err)
+	}
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status":"ok"}`)
 }

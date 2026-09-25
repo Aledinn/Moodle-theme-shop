a65d145bddf760af706c0daa8b2202dbb4f03593
Aledinn <Aledinn@users.noreply.github.com>
2026-09-25 10:04:17 +0200

    fix typo and forgotten assignment

--- apps/backend/internal/themes/database.go
+++ apps/backend/internal/themes/database.go
@@ -46,8 +46,7 @@ func RoachDb(ctx context.Context) (*sql.DB, error) {
 		pgConnString += fmt.Sprintf(" password=%s", password)
 	}
 
-
-	db, err = sql.Open("postgres", pgConnString)
+	db, err := sql.Open("postgres", pgConnString)
 	if err != nil {
 		return nil, fmt.Errorf("open database :%w", err)
 	}
@@ -58,10 +57,10 @@ func RoachDb(ctx context.Context) (*sql.DB, error) {
 		return db.PingContext(ctx)
 	}
 
-	err = backoff.Retry(operation,backoff.WithContext(retryPolicy,ctx))
+	err = backoff.Retry(operation, backoff.WithContext(retryPolicy, ctx))
 	if err != nil {
 		db.Close()
-		return nil,fmt.Errorf("connect to database: %w"´,err)
+		return nil, fmt.Errorf("connect to database: %w", err)
 	}
 	return db, nil
 }
